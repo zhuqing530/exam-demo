3a16c7800a3232d3296bf97bc5f6208508bc22b7
zhuqing530 <275139842@qq.com>
2017-11-14 14:52:38 +0800

    defined preDefinedAnswers

---
+++ index.js
@@ -0,0 +1,16 @@
+const preDefinedAnswers = [{
+    unitMark: 5,
+    answers: [["统一建模语言"], ["封装性", "继承性", "多态性"]]
+}, {
+    unitMark: 10,
+    answers: ["B", "A"]
+}, {
+    unitMark: 10,
+    answers: [["A", "B", "D"], ["A", "B", "C"]]
+}, {
+    unitMark: 10,
+    answers: [false, true]
+}, {
+    unitMark: 20,
+    answers: ["模型是对现实世界的简化和抽象，模型是对所研究的系统、过程、事物或概念的一种表达形式。可以是物理实体；可以是某种图形；或者是一种数学表达式。"]
+}];
